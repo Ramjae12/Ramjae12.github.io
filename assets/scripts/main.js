4f7a23e76e2af39be313ff64710134e1772d494b
Ramjae12 <ramjae3@gmail.com>
2025-03-02 12:44:23 +0800

    first commit

---
+++ assets/scripts/main.js
@@ -0,0 +1,98 @@
+// const body = document.body
+// body.append("Hello World", "Dev 101")
+// const div = document.createElement("div")
+// body.append(div)
+
+// div.innerText = "Hello Developers"
+// div.textContent = "Hello Developers!!!"
+// div.innerHTML = "<strong> EYYYY </strong>"
+
+// const strong = document.createElement ("strong")
+//strong.innerHTML = "EYYYOW Hackers"
+//div.append(strong)
+
+const div = document.querySelector('div')
+const spanHi = document.querySelector ('#hi')
+const spanBye = document.querySelector('#bye')
+
+spanHi.style.color = "blue"
+spanHi.style.backgroundColor = "red"
+
+// spanHi.classList.add("new-class")
+// spanHi.classList.remove("hi1")
+// spanHi.classList.toggle("hi1")
+
+
+// console.log(spanHi.dataset)
+// spanHi.dataset.newName = "new name value"
+// spanBye.remove()
+// div.append(spanBye)
+
+// console.log(spanHi.getAttribute("title"))
+// spanHi.setAttribute("id", "change-id")
+// spanHi.removeAttribute("id")
+
+// console.log(spanHi.dataset)
+
+
+
+
+//console.log("Hello World!")
+//console.log("Welcome to WebDev 101")
+
+//let variableName = "Ram Sanchez"
+//const variableName = [1,'Bob','Steve', 10, false, 'Ram']
+//console.log(variableName[variableName.length -1])>
+
+//const firstNum = 50
+//const secNum = 2
+//const thirdNum = '3'
+//const sum = firstNum + secNum
+//const diff = firstNum - secNum
+//const prod = firstNum * secNum
+//const quot = firstNum / secNum
+//const exponent = firstNum ** secNum
+//const reminder = firstNum % secNum
+
+//console. log(typeof(thirdNum))
+
+//console.log ( 50 % 3)
+
+//console.log (firstNum + thirdNum)
+
+// function myFunc() {
+//    return "My function is working"
+ //}
+ // console.log(myFunc())
+
+ //if (true) {
+ //     console.log("I am working")
+ //} else {
+ //  console.log ("Everything that is false will fall here")
+// }
+
+//const iceCream = "EY"
+
+//if (iceCream === "Chocolate") {
+ //  console.log("This is my fav ice cream")
+//}  else if (iceCream === "Matcha")
+ //     console.log("I like this too mafriend")
+ //  else {
+  //    console.log("I don't like this ice cream")
+//}
+
+//for(let x = 1; x <= 100; x++ ) {
+  // if(x % 2 != 0)
+  // console.log (x)
+//}
+
+//x = x + 1 === x++
+//x = x - 1 === x--
+//x = x + 5 === x-+5
+
+//let x = 0 
+
+//while (x <= 100) {
+//   console.log(x)
+   //x++
+//}
